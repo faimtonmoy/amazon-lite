d06ab425d653d4ad3461017c65868b3ec1f1b95c
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-10-08 09:40:04 +0600

    inventory added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Inventory from './components/Inventories/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route exact path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route exact path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route exact path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
